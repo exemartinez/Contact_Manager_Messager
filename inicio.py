2bdf705309bcc62fa37ddae982f1bdc574b64d42
hernanezequielmartinez <hernanezequielmartinez@gmail.com>
2016-11-16 19:32:21 -0300

    Added a new REST methos template.

--- inicio.py
+++ inicio.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def index():
     '''Establishes the app entry point.'''
     return render_template("home.html")
@@ -11,15 +13,29 @@ def message():
     '''Sets up a message and selects the recipes.'''
     return render_template("message.html")
 
-@app.route('/inboxSelection')
-def inboxSelection():
-    '''Shows the available recipes.'''
-    return render_template("inboxSelection.html")
 
-@app.route('/inboxes')
-def prueba():
-    '''Shows the available recipes.'''
-    return render_template("inboxes.html")
+#**************
+# REST backend
+#**************
+@app.route('/api/v1.0/contacts/all', methods=['GET'])
+def get_all_contacts():
+    '''Returns all the database contacts.'''
+    #TODO: Have to implement this method ASAP.
+    resultado = "Completar con la funcionalidad adecuada!"
+    return jsonify({'resultado': { \
+    'resulta': resultado \
+    }}),200
+    #return jsonify(resultado)
+
+@app.route('/api/v1.0/contacts/load/linkedin/<string:dataFile>', methods=['GET'])
+def post_load_linkedin_contacts(dataFile):
+    '''Loads a all the database contacts.'''
+    #TODO: Have to implement this method ASAP.
+    resultado = "Completar con la funcionalidad de carga del archivo desde linkedin!" + dataFile
+    return jsonify({'resultado': { \
+    'resulta': resultado \
+    }}), 200
+    #return jsonify(resultado)
 
 if __name__ == "__main__":
     app.run()
