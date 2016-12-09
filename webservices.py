30638ac672ad9b00762b0500f47e0331d2195dd9
hernanezequielmartinez <hernanezequielmartinez@gmail.com>
2016-12-09 18:21:38 -0300

    upload b backend implemented.

--- webservices.py
+++ webservices.py
@@ -1,8 +1,18 @@
-from flask import Flask, render_template, jsonify, request, json
+from flask import Flask, render_template, jsonify, request, json, redirect, url_for
+from flask import send_from_directory
+import os
+from werkzeug.utils import secure_filename
 from backend import ImportController, ContactosController, MessagingController, LogMngr
 
+#Constants
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif','doc','csv','docx'])
+
+#Flask Settings
 app = Flask(__name__)
 log = LogMngr("web services frontend")
+app.config['MAX_CONTENT_LENGTH'] = 64 * 1024 * 1024 #it allows to upload a file of up to 64mb.
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 @app.route('/home')
@@ -10,21 +20,64 @@ def index():
     '''Establishes the app entry point.'''
     return render_template("home.html")
 
-@app.route('/message')
-def message():
-    '''Sets up a message and selects the recipes.'''
-    return render_template("message.html")
-    
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    '''Returns the uploaded file name'''
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+def allowed_file(filename):
+    '''Defines which sort of files are allowed to be sent to the server'''
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/upload_controller', methods=['GET', 'POST'])
+def upload_file():
+    '''Manages the file after it is sent by a HTML input for uploading.'''
+    log.info("Uploading the posted file...")
+
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            log.error("No file part")
+            flash('No file part')
+            return redirect(request.url)
+
+        file = request.files['file']
+
+        # if user does not select file, browser also submit a empty part without filename
+        if file.filename == '':
+            log.error("No selected file")
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+
+            log.info("Saving file to disk...")
+            try:
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            except Exception as e:
+                log.error("Internal Error:" + str(e))
+                return jsonify({'resultado': 'Error trying to upload the file to the server.'}), 500
+
+            log.info("File uploaded successfully!")
+
+            return jsonify({'resultado': str(url_for('uploaded_file', filename=filename))}), 200
+            #return redirect(url_for('uploaded_file',filename=filename)) #use this in case you want to show the file.
+
+    return jsonify({'resultado': 'Wrong form method or signature.'}), 500
+
 @app.route('/upload')
 def upload():
     '''renders a file uploader page.'''
     return render_template("uploads.html")
 
+@app.route('/message')
+def message():
+    '''Sets up a message and selects the recipes.'''
+    return render_template("message.html")
 
 #**************
 # REST backend
 #**************
-
 @app.route('/api/v1.0/mailing/send', methods=['POST'])
 def send_mail_to_contacts():
     '''Returns all the database contacts.'''
